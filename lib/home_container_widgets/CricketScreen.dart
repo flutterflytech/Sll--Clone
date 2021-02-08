2179ea3e681036e444babe371ccab2d6bfe916e1
mobcoder-saurav <sauravbansal448@gmail.com>
2021-02-08 17:58:50 +0530

    create UI for news feeds

--- lib/home_container_widgets/CricketScreen.dart
+++ lib/home_container_widgets/CricketScreen.dart
@@ -21,7 +21,7 @@ class _CricketScreenWidget extends State<CricketScreen> {
       body: SingleChildScrollView(
         physics: AlwaysScrollableScrollPhysics(),
         child: Container(
-          color: Colors.black87,
+          color: Colors.black38,
           child: Column(
             children: [
               Padding(
@@ -106,6 +106,97 @@ class _CricketScreenWidget extends State<CricketScreen> {
                       ),
                     );
                   }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 12.0, top: 12.0, bottom: 12.0),
+                    child: Text(
+                      'News',
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12.0, right: 12.0),
+                    child: Text(
+                      'View All',
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+              ListView.builder(
+                itemCount: 3,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (BuildContext ctx, int index) {
+                  return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: Container(
+                          color: Colors.black38,
+                          height: 300,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Container(
+                                height: 200,
+                                child: Image.network(
+                                  'https://swall.teahub.io/photos/small/56-567552_marshmallow-whatsapp-dp.jpg',
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 12.0, left: 12.0),
+                                child: Text(
+                                  'IPL 2021 Auction to be held on February 18',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 30.0, left: 12.0, right: 12.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.timer,
+                                            color: Colors.white, size: 15.0),
+                                        Text(
+                                          'Wed, January 27 - 10:55 pm',
+                                          style: TextStyle(
+                                              fontSize: 10.0,
+                                              color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                    Icon(
+                                      Icons.share,
+                                      color: Colors.white,
+                                      size: 17.0,
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ));
+                },
+              )
             ],
           ),
         ),
